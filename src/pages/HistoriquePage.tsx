38871903cf090782f298e56387f62fa4e02d8c9f
Pilpress31 <pilpress31@gmail.com>
2026-05-31 08:34:43 +0200

    HistoriquePage : 8px d'air entre pagination et BottomNav

--- src/pages/HistoriquePage.tsx
+++ src/pages/HistoriquePage.tsx
@@ -252,7 +252,7 @@ function HistoriquePage() {
       {/* Pagination */}
       <div
         className="fixed left-0 w-full flex justify-center items-center gap-4 bg-white py-2 shadow-md z-50"
-        style={{ bottom: "var(--bottom-nav-total, 90px)" }}
+        style={{ bottom: "calc(var(--bottom-nav-total, 90px) + 8px)" }}
       >
         <button
           onClick={() => {
